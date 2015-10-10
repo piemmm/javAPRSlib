3551a04f939c1767aba5c7e79757d86a560a23f5
Ian Hawkins <ian@prowl.org>
2015-10-10 01:51:17 +0100

    Increase test coverage of APRSPacket

--- src/test/java/org/prowl/aprslib/parser/APRSPacketTest.java
+++ src/test/java/org/prowl/aprslib/parser/APRSPacketTest.java
@@ -4,7 +4,9 @@
 package org.prowl.aprslib.parser;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,7 +32,7 @@ public class APRSPacketTest {
 
    @BeforeClass
    public static void setup() throws Exception {
-      DIGIPEATERS.add(new Digipeater(DIGI_CALL_1 + "-" + DIGI_SSID_1));
+      DIGIPEATERS.add(new Digipeater(DIGI_CALL_1 + "-" + DIGI_SSID_1 + "*"));
       DIGIPEATERS.add(new Digipeater(DIGI_CALL_2_Q));
       DIGIPEATERS.add(new Digipeater(DIGI_CALL_3_IGATE));
       INFORMATIONFIELD = new ObjectPacket(";PD1BLU   *000000z5200.99N/00510.80E-Bart".getBytes());
@@ -112,78 +114,90 @@ public class APRSPacketTest {
       p.setDigipeaters(null);
       assertNull(p.getDigipeaters());
    }
-   //
-   // /**
-   // * Test method for {@link org.prowl.aprslib.parser.APRSPacket#getLastUsedDigi()}.
-   // */
-   // @Test
-   // public void testGetLastUsedDigi() {
-   // fail("Not yet implemented");
-   // }
-   //
-   // /**
-   // * Test method for {@link org.prowl.aprslib.parser.APRSPacket#getDigiString()}.
-   // */
-   // @Test
-   // public void testGetDigiString() {
-   // fail("Not yet implemented");
-   // }
-   //
-   // /**
-   // * Test method for {@link org.prowl.aprslib.parser.APRSPacket#getDti()}.
-   // */
-   // @Test
-   // public void testGetDti() {
-   // fail("Not yet implemented");
-   // }
-   //
-   // /**
-   // * Test method for {@link org.prowl.aprslib.parser.APRSPacket#getAprsInformation()}.
-   // */
-   // @Test
-   // public void testGetAprsInformation() {
-   // fail("Not yet implemented");
-   // }
-   //
-   // /**
-   // * Test method for {@link org.prowl.aprslib.parser.APRSPacket#isAprs()}.
-   // */
-   // @Test
-   // public void testIsAprs() {
-   // fail("Not yet implemented");
-   // }
-   //
-   // /**
-   // * Test method for {@link org.prowl.aprslib.parser.APRSPacket#hasFault()}.
-   // */
-   // @Test
-   // public void testHasFault() {
-   // fail("Not yet implemented");
-   // }
-   //
-   // /**
-   // * Test method for {@link org.prowl.aprslib.parser.APRSPacket#setHasFault(boolean)}.
-   // */
-   // @Test
-   // public void testSetHasFault() {
-   // fail("Not yet implemented");
-   // }
-   //
-   // /**
-   // * Test method for {@link org.prowl.aprslib.parser.APRSPacket#getType()}.
-   // */
-   // @Test
-   // public void testGetType() {
-   // fail("Not yet implemented");
-   // }
-   //
-   // /**
-   // * Test method for {@link org.prowl.aprslib.parser.APRSPacket#setType(org.prowl.aprslib.parser.APRSTypes)}.
-   // */
-   // @Test
-   // public void testSetType() {
-   // fail("Not yet implemented");
-   // }
+
+   /**
+    * Test method for {@link org.prowl.aprslib.parser.APRSPacket#getLastUsedDigi()}.
+    */
+   @Test
+   public void testGetLastUsedDigi() {
+      assertEquals(DIGI_CALL_1, APRSPACKET.getLastUsedDigi());
+   }
+
+   /**
+    * Test method for {@link org.prowl.aprslib.parser.APRSPacket#getDigiString()}.
+    */
+   @Test
+   public void testGetDigiString() {
+      assertEquals(DIGI_CALL_1 + "-" + DIGI_SSID_1 + "*," + DIGI_CALL_2_Q + "," + DIGI_CALL_3_IGATE, APRSPACKET.getDigiString());
+   }
+
+   /**
+    * Test method for {@link org.prowl.aprslib.parser.APRSPacket#getDti()}.
+    */
+   @Test
+   public void testGetDti() {
+      assertEquals(';', APRSPACKET.getDti());
+   }
+
+   /**
+    * Test method for {@link org.prowl.aprslib.parser.APRSPacket#getAprsInformation()}.
+    */
+   @Test
+   public void testGetAprsInformation() {
+      assertEquals(INFORMATIONFIELD, APRSPACKET.getAprsInformation());
+   }
+
+   /**
+    * Test method for {@link org.prowl.aprslib.parser.APRSPacket#isAprs()}.
+    */
+   @Test
+   public void testIsAprs() {
+      assertTrue(APRSPACKET.isAprs());
+   }
+
+   /**
+    * Test method for {@link org.prowl.aprslib.parser.APRSPacket#hasFault()}.
+    */
+   @Test
+   public void testHasFault() {
+      assertFalse(APRSPACKET.hasFault());
+   }
+
+   /**
+    * Test method for {@link org.prowl.aprslib.parser.APRSPacket#setHasFault(boolean)}.
+    */
+   @Test
+   public void testSetHasFault() {
+      APRSPacket p = new APRSPacket(TEST_CALL_1, TEST_CALL_2, DIGIPEATERS, INFORMATIONFIELD);
+      assertFalse(p.hasFault());
+      p.setHasFault(true);
+      assertTrue(p.hasFault());
+      p.setHasFault(false);
+      assertFalse(p.hasFault());
+
+   }
+
+   /**
+    * Test method for {@link org.prowl.aprslib.parser.APRSPacket#getType()}.
+    */
+   @Test
+   public void testGetType() throws Exception {
+      assertNull(APRSPACKET.getType());
+      APRSPacket p = Parser.parseBody(TEST_CALL_1, TEST_CALL_2, DIGIPEATERS, new String(INFORMATIONFIELD.rawBytes));
+      assertEquals(APRSTypes.T_OBJECT, p.getType());
+   }
+
+   /**
+    * Test method for {@link org.prowl.aprslib.parser.APRSPacket#setType(org.prowl.aprslib.parser.APRSTypes)}.
+    */
+   @Test
+   public void testSetType() {
+      APRSPacket p = new APRSPacket(TEST_CALL_1, TEST_CALL_2, DIGIPEATERS, INFORMATIONFIELD);
+      assertNull(p.getType());
+      p.setType(APRSTypes.T_KILL);
+      assertEquals(APRSTypes.T_KILL, p.getType());
+   }
+
    //
    // /**
    // * Test method for {@link org.prowl.aprslib.parser.APRSPacket#getOriginalString()}.
